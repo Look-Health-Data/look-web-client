e4b485102c8769caf231cfe09b23ecf69978da35
joeflack4 <joeflack4@gmail.com>
2018-01-28 17:32:26 -0500

    (1) Renamed submodule. (2) Changed modular directory structure. (3) Moved public website to react. (4) Introduced react router.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
+// Imports.Core
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// Imports.Components
+// import { Button } from '@progress/kendo-react-buttons';
+
+// App
 class App extends Component {
+
+  // Boilerplate.Kendo.Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'HH: Dashboard'
+    }
+  }
+
+  // Boilerplate.Kendo.Events
+  // onButtonClick = () => {
+  //   this.setState({title: 'Hello there!'})
+  // };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
 }
 
 export default App
+
+// - Deprecated
+//   render() {
+//     return (
+//       <div className="App">
+//         {/*<header className="App-header">*/}
+//           {/*<img src={logo} className="App-logo" alt="logo" />*/}
+//           {/*<h1 className="App-title">Welcome to Health Hub</h1>*/}
+//         {/*</header>*/}
+//         {/*<p className="App-intro">*/}
+//           {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+//         {/*</p>*/}
+//
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">{this.state.title}</h1>
+//         </header>
+//         <p className="App-intro">
+//           <Button
+//             onClick={this.onButtonClick}
+//             primary={true}
+//             icon={''}
+//             iconClass={''}
+//             imageUrl={''}
+//             imageAlt={''}
+//           >
+//             Clicky Clicky
+//           </Button>
+//         </p>
+//
+//       </div>
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
+// - Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import { default as PublicSite} from './site/PublicSite';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// - Authentication
+let loggedIn = false;
+
+// - Routes
+const BasicExample = () => (
+  <Router>
+    <div>
+      {/*<ul>*/}
+        {/*<li><Link to="/">Home</Link></li>*/}
+        {/*<li><Link to="/about">About</Link></li>*/}
+        {/*<li><Link to="/topics">Topics</Link></li>*/}
+      {/*</ul>*/}
+      {/*<hr/>*/}
+        {/*  TODO: Instead, reroute to /app if logged in. */}
+        {
+          loggedIn
+            ? <Route exact path="/" component={App}/>
+            : <Route exact path="/" component={PublicSite}/>
+        }
+      <Route exact path="/app" component={App}/>
+      <Route path="/site" component={PublicSite}/>
+    </div>
+  </Router>
+);
+// export default BasicExample
+
+// - Render
+ReactDOM.render(<BasicExample />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// - Service Workers
 // lregisterServiceWorker();  // Enable on first, MVP deployment.
+
+// - TODO
+//   - React Router Links
+// import {Link} from 'react-router-dom';
+// <Link to={`${match.url}/rendering`}>
+//   Rendering with React
+// </Link>
