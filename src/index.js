7697e74e502a5eff515faeffe2f668e7a1442055
joeflack4 <joeflack4@gmail.com>
2018-02-15 23:27:10 -0500

    WIP. General update. Creating layout.

--- src/index.js
+++ src/index.js
@@ -1,71 +1,31 @@
 // - Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Helmet from 'react-helmet';
-// import Script from 'react-load-script';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-import App from './App';
-import { default as PublicSite} from './site/PublicSite';
-// import './index.css';
+import { ApolloProvider } from 'react-apollo';
+// import store2 from './site/src/store';
+import store from './app/src/store';
+import SiteHome from './site/src/App';
+import AppHome from './app/src/Home';
+import 'bootstrap/dist/css/bootstrap.css';
 // import registerServiceWorker from './registerServiceWorker';
 
-// - Authentication
 let loggedIn = false;
 
-// - Routes
-const Client = () => (
-    <div>
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>~ Health Hub ~</title>
-            {/*<link rel="canonical" href="http://mysite.com/example" />*/}
-        </Helmet>
-        <Router>
-              <div>
 
-              {/*<ul>*/}
-                {/*<li><Link to="/">Home</Link></li>*/}
-                {/*<li><Link to="/about">About</Link></li>*/}
-                {/*<li><Link to="/topics">Topics</Link></li>*/}
-              {/*</ul>*/}
-              {/*<hr/>*/}
-                {/*  TODO: Instead, reroute to /app if logged in. */}
-                {/*{*/}
-                  {/*loggedIn*/}
-                    {/*? <Route exact path="/" component={App}/>*/}
-                    {/*: <Route exact path="/" component={PublicSite}/>*/}
-                {/*}*/}
-              {/*<Route path={!loggedIn ? '/app' : '/app'} component={App}/>*/}
-              <Route exact path='/' component={loggedIn ? App : PublicSite}/>
-
-              <Route path='/app' component={App}/>
-              <Route path='/site' component={PublicSite}/>
-                  </div>
-
-                {/*<Route path={loggedIn ? '/site' : '/(|site)'} component={PublicSite}/>*/}
-        </Router>
-        {/*<Script url=''/>*/}
-		{/*<Script url="static/js/jquery.min.js"/>*/}
-		{/*<Script url="static/js/browser.min.js"/>*/}
-		{/*<Script url="static/js/breakpoints.min.js"/>*/}
-		{/*<Script url="static/js/util.js"/>*/}
-		{/*<Script url="static/js/main.js"/>*/}
-    </div>
-
-
-
-);
-
-// - Render
-ReactDOM.render(<Client />, document.getElementById('root'));
+// - App
+ReactDOM.render(
+  <ApolloProvider client={store}>
+    <Router>
+      <div>
+        <Route exact path='/' component={loggedIn ? AppHome : SiteHome}/>
+        <Route path='/app' component={AppHome}/>
+        <Route path='/site' component={SiteHome}/>
+      </div>
+    </Router>
+  </ApolloProvider>,
+  
+  document.getElementById('root'));
 
 // - Service Workers
 // lregisterServiceWorker();  // Enable on first, MVP deployment.
-
-// - TODO
-//   - React Router Links
-// import {Link} from 'react-router-dom';
-// <Link to={`${match.url}/rendering`}>
-//   Rendering with React
-// </Link>
