4fc85bb7b49b6f4aef99cf3f43d6f878524c6c7e
joeflack4 <joeflack4@gmail.com>
2018-03-12 00:59:17 -0400

    Added initial version of Health Domains page.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
+// noinspection NpmUsedModulesInstalled
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import store2 from './site/src/store';
 import store from './app/src/store';
 import SiteHome from './site/src/Site';
 import AppHome from './app/src/Home';
 import NotFound404 from './app/src/views/NotFound404'
-import { GS1, GS2, GS3, GS4,
-  GettingStartedPersonType, GettingStartedWhoFor, GettingStartedTest }
-  from './app/src/GettingStarted'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
+import {
+  GettingStartedPersonType, GettingStartedTest, GettingStartedWhoFor
+} from "./app/src/views/GettingStarted/_ignored/GettingStartedIgnored";
+import GS1 from "./app/src/views/GettingStarted/pages/1";
+import GS2 from "./app/src/views/GettingStarted/pages/2";
+import GS3 from "./app/src/views/GettingStarted/pages/3";
+import GS4 from "./app/src/views/GettingStarted/pages/4";
+import GS5 from './app/src/views/GettingStarted/pages/5';
 // import registerServiceWorker from './registerServiceWorker';
 
 let loggedIn = false;
@@ -22,29 +29,32 @@ let loadSite = loggedIn;
 // - App
 ReactDOM.render(
   <ApolloProvider client={store}>
-    <Router>
-      {/*<div className={loadSite ? '' : 'outer'}>*/}
-      <div className='outer'>
-      {/*<div>*/}
-      <Switch>
-        <Route exact path='/' component={loadSite ? AppHome : SiteHome}/>
-        <Route path='/home' component={AppHome}/>
-        <Route path='/site' component={SiteHome}/>
-        {/*This route instead somehow causes getting-started/* to return GettingStarted1*/}
-        {/*<Route path='/getting-started/' component={GettingStarted1}/>*/}
-        <Route path='/getting-started/who-for' component={GettingStartedWhoFor}/>
-        <Route path='/getting-started/person-type' component={GettingStartedPersonType}/>
-        <Route path='/getting-started/test' component={GettingStartedTest}/>
-        
-        <Route path='/getting-started/1' component={GS1}/>
-        <Route path='/getting-started/2' component={GS2}/>
-        <Route path='/getting-started/3' component={GS3}/>
-        <Route path='/getting-started/4' component={GS4}/>
-        
-        <Route component={NotFound404} />
-      </Switch>
-      </div>
-    </Router>
+    <MuiThemeProvider>
+      <Router>
+        {/*<div className={loadSite ? '' : 'outer'}>*/}
+        {/*<div className='outer'>*/}
+        {/*<div>*/}
+        <Switch>
+          <Route exact path='/' component={loadSite ? AppHome : SiteHome}/>
+          <Route path='/home' component={AppHome}/>
+          <Route path='/site' component={SiteHome}/>
+          {/*This route instead somehow causes getting-started/* to return GettingStarted1*/}
+          {/*<Route path='/getting-started/' component={GettingStarted1}/>*/}
+          <Route path='/getting-started/who-for' component={GettingStartedWhoFor}/>
+          <Route path='/getting-started/person-type' component={GettingStartedPersonType}/>
+          <Route path='/getting-started/test' component={GettingStartedTest}/>
+          
+          <Route path='/getting-started/1' component={GS1}/>
+          <Route path='/getting-started/2' component={GS2}/>
+          <Route path='/getting-started/3' component={GS3}/>
+          <Route path='/getting-started/4' component={GS4}/>
+          <Route path='/getting-started/5' component={GS5}/>
+          
+          <Route component={NotFound404} />
+        </Switch>
+        {/*</div>*/}
+      </Router>
+    </MuiThemeProvider>
   </ApolloProvider>,
   
   document.getElementById('root'));
